2f448211a3c1a1e5283cabca46db13a8ae216cc9
tecclasdev1 <100302650+tecclasdev1@users.noreply.github.com>
2022-09-12 16:46:44 -0500

    Primeros cambios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [segundos, setSegundos] = useState(0);
+
+  const [minutos, setMinutos] = useState(0);
+  const [milisegundos, setMilisegundos] = useState(0);
+
+  const [activarContador, setActivarContador] = useState(false);
+  const intervalRef = useRef();
+
+  const cambiarEstadoContador = () => {
+    let x = !activarContador;
+     setActivarContador(x); 
+
+    /*    x === true
+      ? (intervalRef.current = setInterval(() => {
+          setSegundos((segundos) => segundos + 1);
+ }, 1000))
+      : clearInterval(intervalRef.current);  */
+
+    if (x === true) {
+      intervalRef.current = setInterval(() => {
+        setMilisegundos((milisegundos) => milisegundos + 1);
+      }, 10);
+    } else if (x === false) {
+      clearInterval(intervalRef.current);
+    }
+  };
+
+  const resetearCronometro = () => {
+    clearInterval(intervalRef.current);
+    setMilisegundos(0);
+    setMinutos(0);
+    setSegundos(0);
+    setActivarContador(false);
+  };
+
+  useEffect(() => {
+   
+
+
+    if (milisegundos > 99) {
+      setMilisegundos(0);
+      setSegundos((segundos) => segundos + 1);
+    }
+    if (segundos > 59) {
+      setSegundos(0);
+      setMinutos((minutos) => minutos + 1);
+    }
+  }, [milisegundos, segundos]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="fs-3">
+        {minutos < 10 ? "0" + minutos : minutos}:
+        {segundos < 10 ? `0` + segundos : segundos}:
+        {milisegundos < 10 ? `0` + milisegundos : milisegundos}
+      </p>
+      <button
+        className={!activarContador ? "btn btn-primary" : "btn btn-danger"}
+        onClick={cambiarEstadoContador}
+      >
+        {!activarContador ? "Iniciar" : "Pausar"}
+      </button>
+      <button className="btn btn-success mx-3" onClick={resetearCronometro}>
+        Reiniciar
+      </button>
+
+      {activarContador.toString()}
     </div>
   );
 }
---
+++ src/Button/button.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Button() {
+  return (
+    <div>
+      <button >Iniciar </button>
+      <button >Pausar </button>
+      <button >Reiniciar </button>
+    </div>
+  );
+}
+
+export default Button;
